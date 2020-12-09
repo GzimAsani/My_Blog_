202f53209c930c8b6a1482ca2752cf7bed40f51b
Gzim <gzimasanni@gmail.com>
2020-12-09 10:45:04 +0100

    Add admin user and admin privilege

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = 'Account and all associated articles successfuly delted!'
     redirect_to articles_path
   end
@@ -59,8 +59,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = 'You can edit/update only your own user'
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = 'You can edit/delete only your own user'
       redirect_to @user
     end
   end
